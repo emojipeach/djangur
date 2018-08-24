e26c94f35f97345a2504f45435d3984a67f76610
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-08-24 19:57:44 +0000

    Update settings.py

--- imageapp/settings.py
+++ imageapp/settings.py
@@ -3,3 +3,14 @@ image_quality_val = 75
 
 """ Set the size of generated thumbnails (width, height)."""
 thumb_size = (320, 320)
+
+""" Set the available expiry options."""
+expiry_choices = (
+    (1, '24 hours'),
+    (7, '7 days'),
+    (30, '30 days'),
+    (60, '60 days'),
+    (180, '180 days'),
+    (365, '1 year'),
+    (-10, 'Never'),
+    )
