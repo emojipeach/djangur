2e499785a3fdab8b4a7bc6ec81ec4546b58cf87d
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-08-26 21:00:02 +0000

    Update views.py

--- imageapp/views.py
+++ imageapp/views.py
@@ -1,35 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from re import search as regex_search
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from uuid import uuid4
-from time import strftime, localtime, time
+from time import time
 
 from .forms import ImageUploadForm
 from .models import ImageUpload
-
-def get_folder_filename(instance):
-    """ Returns a tuple containing folder and filename when given an image instance."""
-    regex = r"^/img/(.+)/([^/]+)$"
-    url = instance.image_file.url
-    match = regex_search(regex, url)
-    folder = match.group(1)
-    filename = match.group(2)
-    return (folder, filename)
-
-def get_filesize(instance):
-    """ Returns a formatted string for use in templates from the image.size attribute provided in bytes."""
-    size_bytes = instance.image_file.size
-    if size_bytes > 1048576:
-        result = "{0:.2f}".format(size_bytes / 1048576) + " MB"
-    elif size_bytes > 1024:
-        result = "{0:.0f}".format(size_bytes / 1024) + " KB"
-    else:
-        result = '{:,}'.format(size_bytes) + " Bytes"
-    return result
 
 def index(request):
     if request.method == 'POST':
@@ -50,23 +29,14 @@ def index(request):
 def image(request, identifier):
     current_image = ImageUpload.objects.get(identifier=identifier)
     
-    if current_image.expiry_time < time() and current_image.expiry_time > current_image.uploaded_time:
+    if current_image.expiry_time() < time() and current_image.expiry_time() > current_image.uploaded_time:
         raise Http404("Page not found")
     
     absolute_file_url = request.build_absolute_uri(current_image.image_file.url)
     
-    filename = get_folder_filename(current_image)[1]
-    
-    filesize = get_filesize(current_image)
-    
-    uploaded_display_time = strftime('%b. %d, %Y, %-I:%M %p', localtime(current_image.uploaded_time))
-    
     context = {
-        'filesize': filesize,
-        'filename': filename,
         'current_image': current_image,
         'absolute_file_url': absolute_file_url,
-        'uploaded_display_time': uploaded_display_time,
     }
     return render(request, 'imageapp/image.html', context)
 
