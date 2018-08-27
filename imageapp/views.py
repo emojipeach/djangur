dc87214d3d1caf3139f54c5292689f2ca377df0d
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-08-27 20:26:59 +0000

    Update views.py

--- imageapp/views.py
+++ imageapp/views.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+import logging
+
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, Http404
+from django.conf import settings
 from django.urls import reverse
 from uuid import uuid4
 from time import time
 
 from .forms import ImageUploadForm
 from .models import ImageUpload
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 
 def index(request):
     if request.method == 'POST':
@@ -18,7 +24,7 @@ def index(request):
             new_image.identifier = uuid4().hex
             new_image.uploaded_time = time()
             new_image.save()
-            return HttpResponseRedirect(reverse('imageapp:image', args=[new_image.identifier]))
+            return HttpResponseRedirect(reverse('imageapp:image', args=[new_image.identifier, new_image.upload_success_password()]))
     else:
         form = ImageUploadForm()
     context = {
@@ -26,8 +32,11 @@ def index(request):
     }
     return render(request, 'imageapp/index.html', context)
 
-def image(request, identifier):
-    current_image = ImageUpload.objects.get(identifier=identifier)
+def image(request, identifier, upload_success_password=''):
+    try:
+        current_image = ImageUpload.objects.get(identifier=identifier)
+    except Exception:
+        raise Http404("Page not found")
     
     if current_image.expiry_time() < time() and current_image.expiry_time() > current_image.uploaded_time:
         raise Http404("Page not found")
@@ -37,6 +46,27 @@ def image(request, identifier):
     context = {
         'current_image': current_image,
         'absolute_file_url': absolute_file_url,
+        'attempted_upload_success_password': upload_success_password,
     }
     return render(request, 'imageapp/image.html', context)
 
+def delete_image(request, identifier, deletion_password=''):
+    try:
+        current_image = ImageUpload.objects.get(identifier=identifier)
+    except Exception:
+        raise Http404("Page not found")
+    
+    filename = current_image.filename
+    
+    if deletion_password == current_image.deletion_password():
+        current_image.delete()
+        os.remove(current_image.image_file.path)
+        os.remove(current_image.thumbnail.path)
+        
+        context ={
+            'filename': filename,
+        }
+        return render(request, 'imageapp/image_deleted.html', context)
+        
+    else:
+        raise Http404("Page not found")
