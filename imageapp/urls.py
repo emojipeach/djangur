38a25357aef854c2dce16e5b490b2f66a79f28dc
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-08-27 20:27:25 +0000

    Update urls.py

--- imageapp/urls.py
+++ imageapp/urls.py
@@ -7,5 +7,7 @@ app_name = 'imageapp'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    path('image/<str:identifier>/<str:upload_success_password>/', views.image, name='image'),
     path('image/<str:identifier>/', views.image, name='image'),
+    path('delete_image/<str:identifier>/<str:deletion_password>/', views.delete_image, name='delete_image'),
 ]
