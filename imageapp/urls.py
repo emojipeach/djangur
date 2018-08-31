ffbeb2014d31cc42886d49e04de7c9874c9d17ac
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-08-31 21:12:51 +0000

    Update urls.py

--- imageapp/urls.py
+++ imageapp/urls.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.urls import path
 
 from . import views
@@ -7,7 +10,11 @@ app_name = 'imageapp'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
-    path('image/<str:identifier>/<str:upload_success_password>/', views.image, name='image'),
+    path('upload/', views.upload, name='upload'),
     path('image/<str:identifier>/', views.image, name='image'),
     path('delete_image/<str:identifier>/<str:deletion_password>/', views.delete_image, name='delete_image'),
+    path('mod_delete_image/<str:identifier>/<str:deletion_password>/', views.mod_delete_image, name='mod_delete_image'),
+    path('mod_image_acceptable/<str:identifier>/<str:deletion_password>/', views.mod_image_acceptable, name='mod_image_acceptable'),
+    path('report_image/<str:identifier>/', views.report_image, name='report_image'),
+    path('mod_queue/', views.mod_queue, name='mod_queue'),
 ]
