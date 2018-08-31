2e8dca5466cceeeadfee9aabdb285b3e08d9de29
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-08-31 21:13:15 +0000

    Update views.py

--- imageapp/views.py
+++ imageapp/views.py
@@ -3,12 +3,11 @@ from __future__ import unicode_literals
 
 import logging
 import os
-import threading
 
+from random import randint
 from time import time
 from uuid import uuid4
 
-from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import render
@@ -16,32 +15,20 @@ from django.urls import reverse
 
 from imageapp.forms import ImageUploadForm
 from imageapp.models import ImageUpload
-from imageapp.settings import expiry_removal_frequency
+from imageapp.settings import moderation_counter_reset
+from imageapp.settings import moderation_threshold
+from imageapp.startup import delete_expired_images
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 
-def delete_expired_images():
-    now = time()
-    images = ImageUpload.objects.filter(uploaded_time__lte=F('expiry_time'), expiry_time__lte=now)
-    for image in images:
-        filename = image.image_file.filename()
-        os.remove(image.image_file.path)
-        os.remove(image.thumbnail.path)
-        image.delete()
-        logging.info('deleted an expired image: {0}'.format(filename))
-
-def launch_expired_image_remover():
-    t = threading.Timer(expiry_removal_frequency, launch_expired_image_remover)
-    t.start()
-    delete_expired_images()
-
 def index(request):
+    """ Default index view which displays some recent images."""
     images = ImageUpload.objects.filter(private=False).order_by('-uploaded_time')[:5]
     context = {'images': images}
     return render(request, 'imageapp/index.html', context)
-    
 
 def upload(request):
+    """ View for the image upload form."""
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
@@ -51,11 +38,8 @@ def upload(request):
             new_image.expiry_time = new_image.get_expiry_time()
             new_image.save()
             
-            absolute_file_url = request.build_absolute_uri(new_image.image_file.url)
-            
             context = {
                 'current_image': new_image,
-                'absolute_file_url': absolute_file_url,
                 'attempted_upload_success_password': new_image.upload_success_password(),
             }
             
@@ -73,14 +57,19 @@ def image(request, identifier):
     except Exception:
         raise Http404("Page not found")
     
+    if current_image.reported >= moderation_threshold:
+        message = 'Image awaiting moderation'
+        context = {
+            'message': message,
+        }
+        return render(request, 'imageapp/result.html', context)
+        
+    
     if current_image.expiry_time < time() and current_image.expiry_time > current_image.uploaded_time:
         raise Http404("Page not found")
     
-    absolute_file_url = request.build_absolute_uri(current_image.image_file.url)
-    
     context = {
         'current_image': current_image,
-        'absolute_file_url': absolute_file_url,
     }
     return render(request, 'imageapp/image.html', context)
 
@@ -89,18 +78,80 @@ def delete_image(request, identifier, deletion_password=''):
         current_image = ImageUpload.objects.get(identifier=identifier)
     except Exception:
         raise Http404("Page not found")
-    
     if deletion_password == current_image.deletion_password():
-        filename = current_image.filename
+        message = current_image.filename() + ' deleted'
         os.remove(current_image.image_file.path)
         os.remove(current_image.thumbnail.path)
         current_image.delete()
-        context ={
-            'filename': filename,
+        context = {
+            'message': message,
         }
-        return render(request, 'imageapp/image_deleted.html', context)
+        return render(request, 'imageapp/result.html', context)
         
     else:
         raise Http404("Page not found")
 
-launch_expired_image_remover()
+def report_image(request, identifier):
+    try:
+        current_image = ImageUpload.objects.get(identifier=identifier)
+    except Exception:
+        raise Http404("Page not found")
+    current_image.reported += 1
+    if current_image.reported_first_time == 0:
+        current_image.reported_first_time = time()
+    current_image.save(update_fields=['reported', 'reported_first_time'])
+    
+    message = 'Image has been reported for moderation'
+    context = {
+            'message': message,
+        }
+    return render(request, 'imageapp/result.html', context)
+
+def mod_delete_image(request, identifier, deletion_password=''):
+    try:
+        current_image = ImageUpload.objects.get(identifier=identifier)
+    except Exception:
+        raise Http404("Page not found")
+    
+    if deletion_password == current_image.deletion_password():
+        os.remove(current_image.image_file.path)
+        os.remove(current_image.thumbnail.path)
+        current_image.delete()
+        return HttpResponseRedirect(reverse('imageapp:mod_queue'))
+        
+    else:
+        raise Http404("Page not found")
+
+def mod_image_acceptable(request, identifier, deletion_password=''):
+    try:
+        current_image = ImageUpload.objects.get(identifier=identifier)
+    except Exception:
+        raise Http404("Page not found")
+    
+    if deletion_password == current_image.deletion_password():
+        current_image.reported = -moderation_counter_reset
+        current_image.reported_first_time = 0
+        current_image.save(update_fields=['reported', 'reported_first_time'])
+        # TODO attribute this action to the mod responsible
+        return HttpResponseRedirect(reverse('imageapp:mod_queue'))
+    else:
+        raise Http404("Page not found")
+        
+
+def mod_queue(request):
+    # get 10 images above moderation_threshold and sort by the first time they were reported
+    images_for_moderation = ImageUpload.objects.filter(reported__gte=moderation_threshold).order_by('-reported_first_time')[:100]
+    # pick a random image from this list to show to moderator
+    try:
+        moderate = images_for_moderation[randint(0, len(images_for_moderation) - 1)]
+    except ValueError:
+        message = 'Moderation queue empty'
+        context = {
+                'message': message,
+            }
+        return render(request, 'imageapp/result.html', context)
+        
+    context = {
+        'moderate': moderate
+    }
+    return render(request, 'imageapp/mod_queue.html', context)
