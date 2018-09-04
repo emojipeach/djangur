fe656758b99817bd37990bcf7eff3305e915e206
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-04 18:34:51 +0000

    Update forms.py

--- imageapp/forms.py
+++ imageapp/forms.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django import forms
 from .models import ImageUpload
 
@@ -13,5 +16,22 @@ class ImageUploadForm(forms.ModelForm):
         fields = (
         'title',
         'image_file', 'expiry_choice',
+        'private'
+        )
+
+class ImageUploadURLForm(forms.ModelForm):
+    
+    image_form_url = forms.CharField(max_length=255)
+    
+    def __init__(self, *args, **kwargs):
+        super(ImageUploadURLForm, self).__init__(*args, **kwargs)
+        self.fields['private'].initial  = True
+        self.fields['expiry_choice'].initial = -10
+    
+    class Meta:
+        model = ImageUpload
+        fields = (
+        'title',
+        'expiry_choice',
         'private'
         )
