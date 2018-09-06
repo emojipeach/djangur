4b25ece7ce29ca1e301341213db4baf4c45b0711
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-06 21:54:22 +0000

    Update urls.py

--- djangur/urls.py
+++ djangur/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^users/', include('users.urls')),
     url(r'', include('imageapp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
