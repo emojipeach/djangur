f5c25a62941ba9235325620f90ed1d59fbe81ba0
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-07 00:04:35 +0000

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import login
-
+from django.contrib.auth.views import LoginView
+ 
 from . import views
-
+ 
 app_name = 'users'
 urlpatterns = [
     # Login page
-    path('login/', login, {'template_name': 'users/login.html'},
+    path('login/', LoginView.as_view(template_name='users/login.html'),
     name = 'login'),
     # Logout page
     path('logout/', views.logout_view,
