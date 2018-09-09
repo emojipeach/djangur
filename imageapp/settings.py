4cbf5bef722adb2b212283faa313374ff9b47c70
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-09 00:25:45 +0000

    Update settings.py

--- imageapp/settings.py
+++ imageapp/settings.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 # Sets the quality % value used to save an image. Default = 75.
-image_quality_val = 5
+IMAGE_QUALITY_VAL = 5
 
 # Set the size of generated thumbnails (width, height).
-thumb_size = (200, 200)
+THUMB_SIZE = (200, 200)
 
 # Set the available expiry options.
-expiry_choices = (
+EXPIRY_CHOICES = (
     (1, '24 hours'),
     (7, '7 days'),
     (30, '30 days'),
@@ -19,13 +19,13 @@ expiry_choices = (
     )
 
 # Sets the frequency at which image expiries are checked and images deleted in seconds.
-expiry_removal_frequency = 3600
+EXPIRY_REMOVAL_FREQUENCY = 3600
 
 # Sets a number of reports above which an image becomes inaccessable and is added to the moderation queue for approval or deletion.
-moderation_threshold = 1
+MODERATION_THRESHOLD = 1
 
 # When an image is moderated the counter will need to reach this number again before it is submitted for reevaluation.
-moderation_counter_reset = 10
+MODERATION_COUNTER_RESET = 10
 
 # Allowed image formats (need to be upper case image formats recognised by pillow.
-allowed_image_formats = ['JPEG', 'GIF', 'PNG']
+ALLOWED_IMAGE_FORMATS = ['JPEG', 'GIF', 'PNG']
