5be8d9c898f456291d42f10ea5900de974dd84ce
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-09 23:02:09 +0000

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth import logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -29,3 +34,22 @@ def register(request):
             return HttpResponseRedirect(reverse('imageapp:index'))
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
+
+@login_required
+def password_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return HttpResponseRedirect(reverse('imageapp:profile', args=[request.user.username]))
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/password_change.html', context)
