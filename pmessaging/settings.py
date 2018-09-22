22196dd7bd9b778f44bf8f6fcdc11db58518be10
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-22 19:15:32 +0000

    Create settings.py

---
+++ pmessaging/settings.py
@@ -0,0 +1,5 @@
+# Detetmines the frequency at which old messages are checked and deleted.
+MESSAGE_REMOVAL_FREQUENCY = 3600
+
+# Determines the numbet of days back old messages are kept in the db. After this time messages are deleted.
+OLD_MESSAGES_DELETED_IN = -30
